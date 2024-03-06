be95dc368cd2fa70e5eadc4705a571bd030a975a
CenturyPos <adm1ncenturypos@gmail.com>
2024-03-05 23:27:03 -0400

    deploy backend

--- config/settings.py
+++ config/settings.py
@@ -54,8 +54,8 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     "django.middleware.security.SecurityMiddleware",
-    "django.contrib.sessions.middleware.SessionMiddleware",
     'whitenoise.middleware.WhiteNoiseMiddleware',
+    "django.contrib.sessions.middleware.SessionMiddleware",
     'corsheaders.middleware.CorsMiddleware',
     "django.middleware.common.CommonMiddleware",
     "django.middleware.csrf.CsrfViewMiddleware",
